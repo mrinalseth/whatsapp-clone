f856c8fb3c6cb5f99ac4675a25b7c2fe762c5c6d
Mrinal Seth <mrinalseth3959@gmail.com>
2021-10-03 01:24:52 +0530

    sidebar added

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
+const firebaseConfig = {
+    apiKey: "AIzaSyDADss9qeEzRmvsgpBd1ISRAYtEoaiWnkk",
+    authDomain: "whatsapp-clone-4232b.firebaseapp.com",
+    projectId: "whatsapp-clone-4232b",
+    storageBucket: "whatsapp-clone-4232b.appspot.com",
+    messagingSenderId: "968343793855",
+    appId: "1:968343793855:web:1a4ea5a5b24fb3a459960a",
+    measurementId: "G-WVMTQ1ZS4R"
+  };
+  
